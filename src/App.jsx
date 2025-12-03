86e9223bb9a755c9f0d09fc17a579682e53fbc03
indrek hints <indrek.hints@mail.ee>
2025-12-03 20:14:54 +0200

    uuendused 3 detsember

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,69 @@
+
+import { useState } from "react";
+
 function App() {
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    phone: ""
+  });
+
+
+
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:5000/api/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(form)
+    });
+
+    const data = await response.json();
+    console.log("Inserted user:", data.phone);
+
+    alert(data.phone);
+
+    setForm({ name: "", email: "", phone: "" });
+  };
+
+
+
   return (
-    <div className="text-3xl font-bold p-10 bg-slate-200 text-slate-900">
-      Tailwind töötab! 😎
-      <p>Updated version control  26.11.2025</p>
-    </div>
+    <form onSubmit={handleSubmit} style={{ maxWidth: 300 }}>
+      <h3>Add New User</h3>
+
+      <input
+        name="name"
+        placeholder="Name"
+        value={form.name}
+        onChange={handleChange}
+      /><br />
+
+      <input
+        name="email"
+        placeholder="Email"
+        value={form.email}
+        onChange={handleChange}
+      /><br />
+
+      <input
+        name="phone"
+        placeholder="Phone"
+        value={form.phone}
+        onChange={handleChange}
+      /><br />
+
+      <button type="submit">Add User</button>
+    </form>
   );
+
 }
 
 export default App;
